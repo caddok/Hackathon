5327e174501a59f84d83785c76305320b0a874a1
Georgi Delchev <g.delchev93@gmail.com>
2018-10-09 11:34:17 +0300

    Added camera feature; R. not working for some reason

--- ProjectHackathon/app/src/main/java/com/example/georgi/projecthackathon/MainActivity.java
+++ ProjectHackathon/app/src/main/java/com/example/georgi/projecthackathon/MainActivity.java
@@ -1,13 +1,116 @@
 package com.example.georgi.projecthackathon;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.georgi.projecthackathon.camera.cam.take.CameraActivity;
+import com.example.georgi.projecthackathon.camera.cam.take.CameraUtils;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.File;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    private Button mTakePicture;
+    private Button mSendPicture;
+    private String imageStoragePath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTakePicture = findViewById(R.id.take_picture_btn);
+        mSendPicture = findViewById(R.id.send_picture_btn);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (!CameraUtils.isDeviceSupportCamera(getApplicationContext())) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Your device doesn't support camera",
+                    Toast.LENGTH_LONG).show();
+            // will close the app if the device doesn't have camera
+            finish();
+        }
+
+        mSendPicture.setOnClickListener(this);
+        mTakePicture.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.take_picture_btn:
+                if (requestCameraPermission()) {
+                    intent = prepFile();
+                    //intent = new Intent(this,CameraActivity.class);
+                    startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+                }
+                break;
+            case R.id.send_picture_btn:
+        }
+    }
+
+    private boolean requestCameraPermission() {
+        final boolean[] isGranted = new boolean[1];
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            isGranted[0] = true;
+                        } else if (report.isAnyPermissionPermanentlyDenied()) {
+                            showPermissionsAlert();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+        return isGranted[0];
+    }
+
+    private void showPermissionsAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissions required!")
+                .setMessage("Camera needs few permissions to work properly. Grant them in settings.")
+                .setPositiveButton("GOTO SETTINGS", (dialog, which) -> CameraUtils.openSettings(MainActivity.this))
+                .setNegativeButton("CANCEL", (dialog, which) -> {
+                }).show();
+    }
+
+    private Intent prepFile() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File file = CameraUtils.getOutputMediaFile();
+        if (file != null) {
+            imageStoragePath = file.getAbsolutePath();
+        }
+
+        Uri fileUri = CameraUtils.getOutputMediaFileUri(getApplicationContext(), file);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        return intent;
+    }
+
 }
---
+++ ProjectHackathon/app/src/main/java/com/example/georgi/projecthackathon/camera/cam/take/CameraActivity.java
@@ -0,0 +1,45 @@
+package com.example.georgi.projecthackathon.camera.cam.take;
+
+
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        Camera deviceCamera = Camera.open();
+
+        Button captureButton = findViewById(R.id.button);
+        captureButton.setOnClickListener(v -> takePicture(deviceCamera));
+    }
+
+    private void takePicture(Camera deviceCamera) {
+        deviceCamera.takePicture(
+                null,
+                null,
+                (data, camera) -> {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                    if (bitmap == null) {
+                        Toast.makeText(CameraActivity.this, "Captured image is empty", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    ImageView capturedImageHolder = findViewById(R.id.captured_image);
+                    capturedImageHolder.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 300, 200, true));
+                });
+    }
+}
